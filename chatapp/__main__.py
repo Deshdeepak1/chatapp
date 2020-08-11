6184a98f04fc1e8c554508fa5d6eed6a5ec0ff31
deshdeepak1 <rkdeshdeepak1@gmail.com>
2020-08-11 21:32:42 +0530

    Updated app

--- chatapp/__main__.py
+++ chatapp/__main__.py
@@ -3,30 +3,10 @@ import os
 from time import sleep
 from chatapp import o,host,port,name
 from .server import server
-    
+from .client import client    
 
-
-
-def client(host,port,name):
-    
-    c=socket.socket()
-    try:
-        c.connect((host,port))
-    except socket.error as e:
-        print(e)
-        exit()
-    
-    c.send(bytes(name,'utf-8'))
-    other=c.recv(1024).decode()
-    print('Chatting with: '+other)
-    print()
-
-    while True:
-        print(other+' : ',c.recv(1024).decode())
-        msg=input(name+' : ')
-        c.send(bytes(msg,'utf-8'))
-
-if o==1:
-    server(port,name)
-elif o==2:
-    client(host,port,name)
+if __name__=='__main__':
+    if o==1:
+        server(port,name)
+    elif o==2:
+        client(host,port,name)
