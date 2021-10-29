c8c8ca1fa0277a775dd0329793602175f6b9a374
Nikhil jain <73842530+nikhiljain-413@users.noreply.github.com>
2021-10-29 22:48:41 +0530

    Update ngrok.py

--- chatapp/ngrok.py
+++ chatapp/ngrok.py
@@ -1,10 +1,8 @@
 from pyngrok import ngrok
 
 
-def Ngrok(port,authtoken):
+def Ngrok(port, authtoken):
     if authtoken:
         ngrok.set_auth_token(authtoken)
-
-    link=ngrok.connect(port,'tcp')
-
+    link = ngrok.connect(port, 'tcp')
     return link
